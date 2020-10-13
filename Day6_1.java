62128eb3bbbe370563fdedfb53c500fac62377e0
Linghu6p <70742821+Linghu6p@users.noreply.github.com>
2020-10-13 13:37:25 +0800

    Day_6 基础知识学习

---
+++ Day6_1.java
@@ -0,0 +1,34 @@
+package Day6;
+
+public class Day6_1 {
+    public Day6_1() {
+        //
+    }
+
+    protected void doSomething() {
+
+    }
+
+    protected Day6_1 dolt() {
+        return new Day6_1();
+    }
+}
+
+class Day_6_1 extends Day6_1 {
+    public Day_6_1() {
+        super();
+        super.doSomething();
+    }
+
+    public void doSomethingNew() {
+
+    }
+    public void doSomething()
+    {
+
+    }
+    protected Day_6_1 dolt()
+    {
+        return new Day_6_1();
+    }
+}
