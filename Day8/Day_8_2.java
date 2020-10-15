892e5806999c27e2e6ec85d82b34c736f973f53b
Linghu6p <70742821+Linghu6p@users.noreply.github.com>
2020-10-15 15:35:14 +0800

    基础知识学习

---
+++ Day8/Day_8_2.java
@@ -0,0 +1,64 @@
+package Day8;
+
+import Day7.*;
+
+import java.util.Scanner;
+
+import static java.lang.Math.max;
+import static java.lang.System.*;
+
+import java.util.Random;
+
+
+class A extends Day_7_3 {
+
+}
+
+class TestOne {
+    int i = 0;
+}
+
+public class Day_8_2 {
+    /*
+    static Random rand = new Random();
+    private final int VALUE_1 = 9;
+    private static final int VALUE_2 = 10;
+    private final TestOne test = new TestOne();
+    private TestOne test2 = new TestOne();
+    protected final int[] a = {1, 2, 3, 4, 5, 6};
+    private final int i4 = rand.nextInt(20);
+    private static final int i5 = rand.nextInt(20);
+
+    public String toString() {
+        return i4 + " " + i5 + " ";
+    }
+
+     */
+
+
+    public static void main(String[] args) {
+        /*
+        int a = max(1,4);
+        out.println(a);
+         */
+
+        //Day_8_2 data = new Day_8_2();
+        /*错误写法
+
+        data.test = new TestOne();
+        data.VALUE_2++;
+
+         */
+        //data.test2 = new TestOne();
+        /*
+
+        out.println("aaa");
+        out.println(data);
+        out.println(new Day_8_2());
+        out.println(data);
+
+         */
+
+
+    }
+}
